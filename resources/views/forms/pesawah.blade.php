affa5dae2c571f4f3159b029355d74cd571b11f6
Suhairi Abdul Hamid <suhairi81@gmail.com>
2021-08-29 04:26:17 +0800

    20210829 01

---
+++ resources/views/forms/pesawah.blade.php
@@ -0,0 +1,88 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Sistem CCS
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200 text-xl">
+                   Input :: Maklumat Pesawah
+                </div>
+                <form class="w-full max-w-sm">
+                  <div class="md:flex md:items-center mb-6 mt-1">
+                    <div class="md:w-1/3">
+                      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                        Nama Pesawah
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text">
+                    </div>
+                  </div>
+                  <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                        No KP
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text">
+                    </div>
+                  </div>
+                  <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                        Taraf Pendidikan
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text">
+                    </div>
+                  </div>
+                  <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                        Status Hak Milik Sawah
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text">
+                    </div>
+                  </div>
+                  <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                        No Tel
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text">
+                    </div>
+                  </div>
+                  <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                        Alamat Rumah
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <textarea class="bg-gray-200 appearance-none border-gray-200 focus:bg-white appearance-none border-2 rounded w-full focus:outline-none leading-tight focus:border-purple-500"></textarea>
+                    </div>
+                  </div>                  
+                  
+                  <div class="md:flex md:items-center">
+                    <div class="md:w-1/3"></div>
+                    <div class="md:w-2/3">
+                      <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                        Hantar
+                      </button>
+                    </div>
+                  </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
