0987c262203546990ab0903eab1acdc869f445fe
Suhairi Abdul Hamid <suhairi81@gmail.com>
2021-09-01 15:42:35 +0800

    20210901 002

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -32,12 +32,8 @@
                         </x-slot>
 
                         <x-slot name="content">
-                            <x-dropdown-link>
-                                <div class="ml-4 text-sm"><a href="{{ route('pesawah') }}">Maklumat Pesawah</a></div>
-                            </x-dropdown-link>
-                            <x-dropdown-link>
-                                <div class="ml-4 text-sm"><a href="{{ route('musim') }}">Maklumat Musim</a></div>
-                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('pesawah')">Pesawah</x-dropdown-link>
+                            <x-dropdown-link :href="route('musim')">Musim</x-dropdown-link>
                         </x-slot>
                     </x-dropdown>
                 </div>
@@ -57,16 +53,11 @@
                         </x-slot>
 
                         <x-slot name="content">
-                            <x-dropdown-link>
-                                <div class="ml-4 text-sm"><a href="{{ route('senaraiPesawah') }}">Pesawah</a></div>
-                            </x-dropdown-link>
-                            <x-dropdown-link>
-                                <div class="ml-4 text-sm"><a href="#">Musim</a></div>
-                            </x-dropdown-link>
+                            <x-dropdown-link :href="route('senaraiPesawah')">Pesawah</x-dropdown-link>
+                            <x-dropdown-link :href="route('dashboard')">Musim</x-dropdown-link>
                         </x-slot>
                     </x-dropdown>
                 </div>
-                
 
                 <div class="hidden sm:flex sm:items-center sm:ml-6">
                     <x-dropdown align="right" width="48">
@@ -83,22 +74,11 @@
                         </x-slot>
 
                         <x-slot name="content">
-                            <x-dropdown-link>
-                                Laporan 1
-                            </x-dropdown-link>
-                            <x-dropdown-link>
-                                Laporan 2
-                            </x-dropdown-link>
-                            <x-dropdown-link>
-                                Laporan 3
-                            </x-dropdown-link>
-                            
+                            <x-dropdown-link :href="route('dashboard')">Laporan 1..</x-dropdown-link>
+                            <x-dropdown-link :href="route('dashboard')">Laporan 2..</x-dropdown-link>
                         </x-slot>
                     </x-dropdown>
                 </div>
-                
-
-                
             </div>
 
             <!-- Settings Dropdown -->
@@ -106,32 +86,28 @@
 
             @if(Auth::user()->id == 1)
                 <div class="pr-4">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                <div>Settings</div>
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                    <div>Users</div>
 
-                                <div class="ml-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                            </button>
-                        </x-slot>
+                                    <div class="ml-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
 
-                        <x-slot name="content">
-                            <x-dropdown-link>
-                                <div class="ml-4 text-sm"><a href="{{ route('users') }}">Users</a></div><!-- add, del, modify -->
-                            </x-dropdown-link>
-                            <x-dropdown-link>
-                                Laporan 2
-                            </x-dropdown-link>
-                            <x-dropdown-link>
-                                Laporan 3
-                            </x-dropdown-link>
-                            
-                        </x-slot>
-                    </x-dropdown>
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('users')">Users</x-dropdown-link>
+                                <x-dropdown-link :href="route('dashboard')">Setting 2..</x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+
+                   
                 </div>
             @endif
                 <x-dropdown align="right" width="48">
@@ -177,9 +153,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+             
         </div>
 
         <!-- Responsive Settings Options -->
